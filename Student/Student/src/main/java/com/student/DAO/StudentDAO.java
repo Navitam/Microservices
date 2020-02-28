076c3f4adbaf3742ac137b5f3d6a745475c7b3d5
Navitam <32322132+Navitam@users.noreply.github.com>
2020-02-28 15:40:05 +0530

    Update StudentDAO.java

--- Student/Student/src/main/java/com/student/DAO/StudentDAO.java
+++ Student/Student/src/main/java/com/student/DAO/StudentDAO.java
@@ -1,12 +1,20 @@
 package com.student.DAO;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.student.Entity.Student;
 
 @Repository
 public interface StudentDAO  extends JpaRepository<Student, Long>{
-
+	
+	
+	@Transactional
+	@Query(value="SELECT * FROM Student student where student.phone_number = :phoneNum", nativeQuery=true) 
+    public Student findByPhoneNum(@Param("phoneNum") Long phoneNumber);
 }
+
